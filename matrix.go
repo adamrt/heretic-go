b233f701039d8eaa64d0e8ef3ad25bfcee110328
Adam Patterson <adam@adamrt.com>
2022-08-24 19:05:04 -0700

    Rename Mat4 to Matrix

--- matrix.go
+++ matrix.go
@@ -2,11 +2,11 @@ package main
 
 import "math"
 
-type Mat4 struct {
+type Matrix struct {
 	m [4][4]float64
 }
 
-func (m Mat4) MulVec4(v Vec4) Vec4 {
+func (m Matrix) MulVec4(v Vec4) Vec4 {
 	return Vec4{
 		m.m[0][0]*v.x + m.m[0][1]*v.y + m.m[0][2]*v.z + m.m[0][3]*v.w,
 		m.m[1][0]*v.x + m.m[1][1]*v.y + m.m[1][2]*v.z + m.m[1][3]*v.w,
@@ -20,8 +20,8 @@ func (m Mat4) MulVec4(v Vec4) Vec4 {
 // | 0  1  0  0 |
 // | 0  0  1  0 |
 // | 0  0  0  0 |
-func Mat4Identity() Mat4 {
-	return Mat4{[4][4]float64{
+func MatrixIdentity() Matrix {
+	return Matrix{[4][4]float64{
 		{1, 0, 0, 0},
 		{0, 1, 0, 0},
 		{0, 0, 1, 0},
@@ -34,8 +34,8 @@ func Mat4Identity() Mat4 {
 // |  0 sy  0  0 |
 // |  0  0 sx  0 |
 // |  0  0  0  1 |
-func Mat4MakeScale(sx, sy, sz float64) Mat4 {
-	m := Mat4Identity()
+func MatrixMakeScale(sx, sy, sz float64) Matrix {
+	m := MatrixIdentity()
 	m.m[0][0] = sx
 	m.m[1][1] = sy
 	m.m[2][2] = sz
@@ -47,8 +47,8 @@ func Mat4MakeScale(sx, sy, sz float64) Mat4 {
 // | 0  1  0  ty |
 // | 0  0  1  tz |
 // | 0  0  0   1 |
-func Mat4MakeTrans(tx, ty, tz float64) Mat4 {
-	m := Mat4Identity()
+func MatrixMakeTrans(tx, ty, tz float64) Matrix {
+	m := MatrixIdentity()
 	m.m[0][3] = tx
 	m.m[1][3] = ty
 	m.m[2][3] = tz
@@ -60,11 +60,11 @@ func Mat4MakeTrans(tx, ty, tz float64) Mat4 {
 // | 0  c -s  0 |
 // | 0  s  c  0 |
 // | 0  0  0  1 |
-func Mat4MakeRotX(angle float64) Mat4 {
+func MatrixMakeRotX(angle float64) Matrix {
 	c := math.Cos(angle)
 	s := math.Sin(angle)
 
-	m := Mat4Identity()
+	m := MatrixIdentity()
 	m.m[1][1] = c
 	m.m[1][2] = -s
 	m.m[2][1] = s
@@ -77,11 +77,11 @@ func Mat4MakeRotX(angle float64) Mat4 {
 // | 0  1  0  0 |
 // |-s  0  c  0 |
 // | 0  0  0  1 |
-func Mat4MakeRotY(angle float64) Mat4 {
+func MatrixMakeRotY(angle float64) Matrix {
 	c := math.Cos(angle)
 	s := math.Sin(angle)
 
-	m := Mat4Identity()
+	m := MatrixIdentity()
 	m.m[0][0] = c
 	m.m[0][2] = s
 	m.m[2][0] = -s
@@ -94,11 +94,11 @@ func Mat4MakeRotY(angle float64) Mat4 {
 // | s  c  0  0 |
 // | 0  0  1  0 |
 // | 0  0  0  1 |
-func Mat4MakeRotZ(angle float64) Mat4 {
+func MatrixMakeRotZ(angle float64) Matrix {
 	c := math.Cos(angle)
 	s := math.Sin(angle)
 
-	m := Mat4Identity()
+	m := MatrixIdentity()
 	m.m[0][0] = c
 	m.m[0][1] = -s
 	m.m[1][0] = s
