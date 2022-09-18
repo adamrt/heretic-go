bddf11de088db94d3cf6f5b9a3a549fbf49e7ce2
Adam Patterson <adam@adamrt.com>
2022-09-18 11:01:40 -0700

    Add Texture.WritePPM

--- texture.go
+++ texture.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"image"
+	"log"
+	"os"
 )
 
 type Texture struct {
@@ -30,4 +34,33 @@ func NewTexture(image image.Image) Texture {
 		height: height,
 		data:   data,
 	}
+}
+
+func (t Texture) WritePPM(filename string) {
+	f, err := os.Create(fmt.Sprintf("./%s", filename))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// Buffered writer for performance
+	bw := bufio.NewWriter(f)
+
+	header := fmt.Sprintf("P3\n%d %d\n16\n", FFTTextureWidth, FFTTextureHeight)
+	_, err = bw.WriteString(header)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(t.data) != FFTTextureSize {
+		log.Fatal("wrong size")
+	}
+	for _, pixel := range t.data {
+		line := fmt.Sprintf("%d %d %d\n", pixel.r, pixel.g, pixel.b)
+		_, err := bw.WriteString(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	bw.Flush()
 }
